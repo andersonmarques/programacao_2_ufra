c49476de45b13ed99c9b6b944d3d150b032aca0c
Anderson Soares <anderson.gmarques@gmail.com>
2022-10-13 01:47:22 -0300

    Atualizando o codigo do projeto de matrizes e GUI

--- trabalhando_matrizes_gui/src/main_view.py
+++ trabalhando_matrizes_gui/src/main_view.py
@@ -13,21 +13,19 @@ class Main_view (QMainWindow, Ui_MainWindow):
         self.push_button_soma_diag_prin.clicked.connect(self.preparar_calculo_diag_prin)
         
     def imprimir_saida(self, texto):
-        self.text_edit_saida.setText(texto)
+        self.text_edit_saida.setText("")
+        self.text_edit_saida.setText(f"{texto}")
 
-    def preparar_calculo_diag_prin(self):
-        self.resultado = self.matriz_controle.somar_diagonal_principal()
-        self.imprimir_saida(self.resultado)
+    def preparar_calculo_diag_prin(self):        
         #capturando a matriz do usuario
-        # matriz_gui = self.text_edit_matriz.toPlainText()
-        # matriz_x = self.matriz_controle.montar_matriz_usuario(matriz_gui)
+        matriz_gui = self.text_edit_matriz.toPlainText()
+        matriz_x = self.matriz_controle.montar_matriz_usuario(matriz_gui)
+        
+        self.resultado = self.matriz_controle.somar_diagonal_principal(matriz_x)
+        self.imprimir_saida(self.resultado)
         # #montando nosso obj matriz
         # matriz_M = Matriz(matriz_x.shape[0],matriz_x.shape[1])   
         # matriz_M.valores = matriz_x
-        # #preprarando o calculo
-        # print(f'na visao:\n{matriz_x}')
-        # #exibindo o resultado
-        # self.text_edit_saida.setText(f'{matriz_M}')
 
 if __name__ == '__main__':
     qt = QApplication(sys.argv)
